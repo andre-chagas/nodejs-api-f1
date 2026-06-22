1502ea5ffc4683ff3b081fcc27e3304eefe8674e
André Chagas <andreluis_chagas@hotmail.com>
2026-06-21 21:11:33 -0300

    feat: primeira versão da API de Fórmula 1

---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+
+const server = fastify({ logger: true });
+
+server.register(cors, {
+  origin: "*",
+});
+
+const teams = [
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
+  { id: 5, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+  { id: 6, name: "Alpine", base: "Enstone, United Kingdom" },
+  { id: 7, name: "Williams", base: "Grove, United Kingdom" },
+  { id: 8, name: "Racing Bulls", base: "Faenza, Italy" },
+  { id: 9, name: "Haas", base: "Kannapolis, United States" },
+  { id: 10, name: "Audi", base: "Hinwil, Switzerland" },
+  { id: 11, name: "Cadillac", base: "Fishers, United States" }
+];
+
+const drivers = [
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+  {id: 1, name: "Lando Norris", team: "MacLaren"},
+];
+
+server.get("/teams", async(request, response) => {
+  response.type("application/json").code(200);
+  return { teams };
+});
+
+server.get("/drivers", async(request, response) => {
+  response.type("application/json").code(200);
+  return { drivers };
+});
+
+interface DriverParams{
+  id: string
+}
+
+server.get<{Params: DriverParams}>("/drivers/:id", async (request, response) => {
+  const id = parseInt(request.params.id);
+  const driver = drivers.find( d => d.id === id);
+
+  if (!driver) {
+    response.type("application/json").code(404);
+    return { message: "Driver Not Found"}
+  } else {
+    response.type("application/json").code(200);
+    return { driver };
+  }
+});
+
+server.listen({ port: 3333 }, () => {
+  console.log("Server init");
+});
